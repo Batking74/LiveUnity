8283d7ae1e549abd42393c605ec43cf76a20c956
Nazir Knuckles <batking74@gmail.com>
2025-07-02 23:30:52 -0400

    Testing

--- client/src/helpers/helpers.js
+++ client/src/helpers/helpers.js
@@ -2,7 +2,7 @@ export let serverUrls = [ 'http://localhost:1000', 'http://localhost:9000' ]
 
 if(location.hostname != 'localhost') {
     serverUrls = [
-        'https://liveunity.onrender.com/api/LiveVisitors',
-        'https://liveunity.onrender.com/api/RoomChat'
+        'https://liveunity.nazirsportfolio.com/api/LiveVisitors',
+        'https://liveunity.nazirsportfolio.com/api/RoomChat'
     ];
 }
